f1c9b5c2c9667469c9c77dadb587141ea070cde5
Charanjeet Singh <charanjeetshaktawat@gmail.com>
2020-09-23 23:51:58 +0530

    Added : Employee Wage for Multiple Companies

--- Employee.java
+++ Employee.java
@@ -3,11 +3,20 @@ import java.util.Scanner;
 
 class Employee {
 	public static void main(String[] args){
-		dailyWage();
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the number of Companies : ");
+		int numOfComp=sc.nextInt();
+		Employee(numOfComp);
 	}
 
-public static void dailyWage(){
+
+public static void Employee(int numofComp){
+	
+	for (int i=1;i<=numofComp;i++){
+		
 	 Scanner sc = new Scanner(System.in);
+	 System.out.println("Enter the Company Name : ");
+	 String compName = sc.nextLine();
 	 System.out.print("Enter the Wage per Hour : ");
 	 int wagePerHr = sc.nextInt();
 	 System.out.print("Enter Full Day Hour : ");
@@ -18,34 +27,52 @@ public static void dailyWage(){
 	 int day=sc.nextInt();
 	 System.out.print("Enter the working hour in a month : ");
 	 int workingHr=sc.nextInt();
-	 calculateWage(wagePerHr,fullDayHr,halfDayHr,day , workingHr);
+	 calculateWage(compName,wagePerHr,fullDayHr,halfDayHr,day , workingHr);
+	 
+	}
 }	
 
 
-public static void calculateWage(int wagePerHr,int fullDayHr,int halfDayHr,int day, int workingHr){
+public static void calculateWage(String compName,int wagePerHr,int fullDayHr,int halfDayHr,int day, int workingHr){
 	int Wage=0;
 	int workHrs=0;
+	int workingDays=0;
 	for (int i=1;i<=day && workHrs<=workingHr;i++){
 			
-			int random=(int)Math.floor(Math.random()*10)%3;
-			switch(random){
+			switch(getrandom()){
 			case 1:
-					Wage = totalWage(wagePerHr,fullDayHr) + Wage;
-					workHrs=workHrs+fullDayHr;
+					Wage = gettotalWage(wagePerHr,fullDayHr,Wage); 
+					workHrs=getWorkingHrs(fullDayHr,workHrs);
 					break;
 			case 2:
-				Wage=totalWage(wagePerHr,halfDayHr)+ Wage;
-				workHrs=workHrs+halfDayHr;
+					Wage=gettotalWage(wagePerHr,halfDayHr,Wage);
+					workHrs=getWorkingHrs(halfDayHr,workHrs);
 				break;
 				default:
 				}
-			System.out.println("Wage : " + Wage + "  Working Hr : " + workHrs + "  Days : " + i );
+			
+			
+			workingDays=i;
 		}
+	System.out.println("Company Name : "+ compName+" Wage : " + Wage + "  Working Hr : " + workHrs + " Working Days : " + workingDays);
 		
 	}
 
-public static int totalWage(int wagePerHr,int dayHr ){
+
+public static int getWorkingHrs(int DayHr , int workHrs){
+	workHrs=DayHr+workHrs;
+	return workHrs;
+}
+
+public static int getrandom(){
+	int random=(int)Math.floor(Math.random()*10)%3;
+	return random;
+}
+
+
+public static int gettotalWage(int wagePerHr,int dayHr,int Wage ){
 	 int dailyWage = wagePerHr*dayHr;
-	 return dailyWage;
+	 Wage=Wage+dailyWage;
+	 return Wage;
 	}
 }
