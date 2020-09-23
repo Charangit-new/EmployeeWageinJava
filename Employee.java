2a1dda078cdaab8ec2e771069f69ffad3a9a9f9e
Charanjeet Singh <charanjeetshaktawat@gmail.com>
2020-09-24 02:39:40 +0530

    Added : Save Total Wage

--- Employee.java
+++ Employee.java
@@ -2,77 +2,87 @@ package employeeWageComputation;
 import java.util.Scanner;
 
 class Employee {
+	
+	int totalWage,wagePerHr,day,workingHr; // instance variable
+	String companyName;
+	
+	// getting number of companies
 	public static void main(String[] args){
 		Scanner sc=new Scanner(System.in);
-		System.out.println("Enter the number of Companies : ");
+		System.out.print("Enter the number of Companies : ");
 		int numOfComp=sc.nextInt();
-		Employee(numOfComp);
-	}
-
-
-public static void Employee(int numofComp){
-	
-	for (int i=1;i<=numofComp;i++){
+		Employee[] CompEmpDetail = new Employee[numOfComp];
 		
-	 Scanner sc = new Scanner(System.in);
-	 System.out.println("Enter the Company Name : ");
-	 String compName = sc.nextLine();
-	 System.out.print("Enter the Wage per Hour : ");
-	 int wagePerHr = sc.nextInt();
-	 System.out.print("Enter Full Day Hour : ");
-	 int fullDayHr=sc.nextInt();
-	 System.out.print("Enter Half Day Hour : ");
-	 int halfDayHr=sc.nextInt();
-	 System.out.print("Enter the days in a month : ");
-	 int day=sc.nextInt();
-	 System.out.print("Enter the working hour in a month : ");
-	 int workingHr=sc.nextInt();
-	 calculateWage(compName,wagePerHr,fullDayHr,halfDayHr,day , workingHr);
-	 
-	}
+	    for (int i=0;i<numOfComp;i++) {
+	    	Scanner sc2=new Scanner(System.in);
+	    	System.out.print("Enter the Company Name : ");
+	    	String compName = sc2.nextLine();
+	    	System.out.print("Enter the Wage per Hour : ");
+	    	int wagePerHr = sc.nextInt();
+	    	System.out.print("Enter Full Day Hour : ");
+	    	int fullDayHr=sc.nextInt();
+	    	System.out.print("Enter Half Day Hour : ");
+	    	int halfDayHr=sc.nextInt();
+	    	System.out.print("Enter the days in a month : ");
+	    	int day=sc.nextInt();
+	    	System.out.print("Enter the working hour in a month : ");
+	    	int workingHr=sc.nextInt();
+	    	
+	    	CompEmpDetail[i]= new Employee(compName,wagePerHr,fullDayHr,halfDayHr,day , workingHr);
+	 }
 }	
 
-
-public static void calculateWage(String compName,int wagePerHr,int fullDayHr,int halfDayHr,int day, int workingHr){
-	int Wage=0;
-	int workHrs=0;
-	int workingDays=0;
-	for (int i=1;i<=day && workHrs<=workingHr;i++){
-			
+public Employee(String compName,int wagePerHr,int fullDayHr,int halfDayHr,int day , int workingHr){
+	this.companyName=compName;
+	this.wagePerHr=wagePerHr;
+	this.day=day;
+	this.workingHr=workingHr;
+	this.totalWage=calculateWage(fullDayHr ,halfDayHr);
+	this.EmpWageBuilder();	
+}
+	
+//calculate total wage for each company
+public  int calculateWage(int fullDayHr , int halfDayHr){
+	int workHrs=0 ,totalWage=0;
+	for (int i=1;i<=this.day && workHrs<=this.workingHr;i++){
 			switch(getrandom()){
 			case 1:
-					Wage = gettotalWage(wagePerHr,fullDayHr,Wage); 
+					totalWage = gettotalWage(wagePerHr,fullDayHr,totalWage); 
 					workHrs=getWorkingHrs(fullDayHr,workHrs);
 					break;
 			case 2:
-					Wage=gettotalWage(wagePerHr,halfDayHr,Wage);
+					totalWage=gettotalWage(wagePerHr,halfDayHr,totalWage);
 					workHrs=getWorkingHrs(halfDayHr,workHrs);
 				break;
 				default:
 				}
-			
-			
-			workingDays=i;
 		}
-	System.out.println("Company Name : "+ compName+" Wage : " + Wage + "  Working Hr : " + workHrs + " Working Days : " + workingDays);
-		
-	}
+	return totalWage;
+}
 
+// Employee Wage Builder
+public void EmpWageBuilder(){
+	System.out.println("Company Name : "+ this.companyName);
+	System.out.println("Wage : " +this.totalWage );
+	System.out.println("************");
+	
+}
 
+// return work hours of an employee in a company
 public static int getWorkingHrs(int DayHr , int workHrs){
 	workHrs=DayHr+workHrs;
 	return workHrs;
 }
-
+// return random value
 public static int getrandom(){
 	int random=(int)Math.floor(Math.random()*10)%3;
 	return random;
 }
 
-
-public static int gettotalWage(int wagePerHr,int dayHr,int Wage ){
+// return total wage
+public static int gettotalWage(int wagePerHr,int dayHr,int totalWage ){
 	 int dailyWage = wagePerHr*dayHr;
-	 Wage=Wage+dailyWage;
-	 return Wage;
+	 totalWage=totalWage+dailyWage;
+	 return totalWage;
 	}
 }
