ee5efcd0c590e4575c3fd46eb1d91c0464da6186
Charanjeet Singh <charanjeetshaktawat@gmail.com>
2020-09-23 23:35:22 +0530

    Added : Conditions

--- Employee.java
+++ Employee.java
@@ -1,12 +1,12 @@
 package employeeWageComputation;
 
 public class Employee {
-	static final int HALFDAY=1,FULLDAY=2,WAGEPERHR=20,MONTH=20;
+	static final int HALFDAY=1,FULLDAY=2,WAGEPERHR=20,MONTH=20,MAXWORKINGHRS=100;
 	public static void main(String[] args)
 	{
 
-		int workingHrs=0,salary,totalSalary=0;
-		for(int day=1;day<=MONTH;day++)
+		int workingHrs=0,salary,totalSalary=0,totalWorkingHrs=0,day;
+		for(day=1;day<=MONTH && totalWorkingHrs<=MAXWORKINGHRS;day++)
 		{
 			int random=(int)Math.floor(Math.random()*10)%3;
 
@@ -22,10 +22,11 @@ public class Employee {
 								workingHrs=0;
 			}
 
+			
+			totalWorkingHrs+=workingHrs;
 			salary=WAGEPERHR*workingHrs;
 			totalSalary+=salary;
 		}
-
 
 		System.out.println("Employee Wage "+totalSalary+" This Month");
 
