8874289d0e6bd5db4fb96f841ce767282f77847a
Charanjeet Singh <charanjeetshaktawat@gmail.com>
2020-09-23 23:45:05 +0530

    Refactor : Class Method and Class Variables

--- Employee.java
+++ Employee.java
@@ -1,34 +1,51 @@
 package employeeWageComputation;
+import java.util.Scanner;
 
-public class Employee {
-	static final int HALFDAY=1,FULLDAY=2,WAGEPERHR=20,MONTH=20,MAXWORKINGHRS=100;
-	public static void main(String[] args)
-	{
+class Employee {
+	public static void main(String[] args){
+		dailyWage();
+	}
 
-		int workingHrs=0,salary,totalSalary=0,totalWorkingHrs=0,day;
-		for(day=1;day<=MONTH && totalWorkingHrs<=MAXWORKINGHRS;day++)
-		{
-			int random=(int)Math.floor(Math.random()*10)%3;
+public static void dailyWage(){
+	 Scanner sc = new Scanner(System.in);
+	 System.out.print("Enter the Wage per Hour : ");
+	 int wagePerHr = sc.nextInt();
+	 System.out.print("Enter Full Day Hour : ");
+	 int fullDayHr=sc.nextInt();
+	 System.out.print("Enter Half Day Hour : ");
+	 int halfDayHr=sc.nextInt();
+	 System.out.print("Enter the days in a month : ");
+	 int day=sc.nextInt();
+	 System.out.print("Enter the working hour in a month : ");
+	 int workingHr=sc.nextInt();
+	 calculateWage(wagePerHr,fullDayHr,halfDayHr,day , workingHr);
+}	
 
-			switch(random)
-			{
-				case HALFDAY:
-								workingHrs=4;
-								break;
-				case FULLDAY:
-								workingHrs=8;
-								break;
-				default:
-								workingHrs=0;
-			}
 
+public static void calculateWage(int wagePerHr,int fullDayHr,int halfDayHr,int day, int workingHr){
+	int Wage=0;
+	int workHrs=0;
+	for (int i=1;i<=day && workHrs<=workingHr;i++){
 			
-			totalWorkingHrs+=workingHrs;
-			salary=WAGEPERHR*workingHrs;
-			totalSalary+=salary;
+			int random=(int)Math.floor(Math.random()*10)%3;
+			switch(random){
+			case 1:
+					Wage = totalWage(wagePerHr,fullDayHr) + Wage;
+					workHrs=workHrs+fullDayHr;
+					break;
+			case 2:
+				Wage=totalWage(wagePerHr,halfDayHr)+ Wage;
+				workHrs=workHrs+halfDayHr;
+				break;
+				default:
+				}
+			System.out.println("Wage : " + Wage + "  Working Hr : " + workHrs + "  Days : " + i );
 		}
+		
+	}
 
-		System.out.println("Employee Wage "+totalSalary+" This Month");
-
+public static int totalWage(int wagePerHr,int dayHr ){
+	 int dailyWage = wagePerHr*dayHr;
+	 return dailyWage;
 	}
 }
